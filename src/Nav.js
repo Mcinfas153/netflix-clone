82141fda3166b9d0064fc5c635f50f1f854cfb11
Mc Infas <mcinfas9394@gmail.com>
2020-12-17 10:06:45 +0400

    mobile responsive added

--- src/Nav.js
+++ src/Nav.js
@@ -18,7 +18,7 @@ function Nav() {
     return (
         <div className={`nav ${navScroll && 'nav__scroll'}`}>
             <img
-                src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+                src="https://download.logo.wine/logo/Netflix/Netflix-Logo.wine.png"
                 className="nav__logo" />
             <img
                 src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
